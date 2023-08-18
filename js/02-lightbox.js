973afa7ec61fd6b66d417cbbe05b63c527e84553
your_space <katerinakaterina182@gmail.com>
2023-08-18 07:34:15 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ const markupGallery = galleryItems.map(
   ({ preview, original, description }) => `
   <li class="gallery__item">
   <a class="gallery__link" href="${original}">
-     <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+     <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>
 </li>`
 );
